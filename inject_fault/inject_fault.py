158a1d7dafab9dcaba4260138c0942dea2e88208
christopher-besch <mail@chris-besch.com>
2026-05-14 12:21:05 +0200

    feat: properly weigh types of faults

--- inject_fault/inject_fault.py
+++ inject_fault/inject_fault.py
@@ -1,14 +1,18 @@
 import skip
+import pathlib
+import json
 import sys
 from random import choice, choices
 import shutil
 
+NEIGHBOUR_DISTANCE = 20
+
 
 def delete_wire(schem) -> str:
     if not hasattr(schem, "wire"):
-        return None
+        assert False
     if len(schem.wire) == 0:
-        return None
+        assert False
     wire = choice(schem.wire)
     changed_thing = f"delete_wire;{wire.start.value}-{wire.end.value}"
     wire.delete()
@@ -17,39 +21,30 @@ def delete_wire(schem) -> str:
 
 # Watch that some swapping is actually okay and shouldn't lead to an error.
 # E.g., using a different GPIO is fine; using a different ADC / ADC channel is fine; as long as everything is connected
-def swap_labels(schem) -> str:
-    if not hasattr(schem, "label"):
-        return None
-    changed_thing = swap(schem.label, schem.label)
-    if changed_thing is None:
-        return None
-    return f"swap_labels;{changed_thing}"
+# Results could be greatly improved by adding simple checks to the Jinja2 template.
+# Right now, e.g., the snippet template only looks for the MISO pin and just assumes that the other ones are correct.
+def swap(schem, first_group_name, second_group_name) -> str:
+    if not hasattr(schem, first_group_name):
+        assert False
+    if not hasattr(schem, second_group_name):
+        assert False
+    first_group = getattr(schem, first_group_name)
+    second_group = getattr(schem, second_group_name)
 
-
-def swap_label_no_connect(schem) -> str:
-    if not hasattr(schem, "label"):
-        return None
-    if not hasattr(schem, "no_connect"):
-        return None
-    changed_thing = swap(schem.no_connect, schem.label)
-    if changed_thing is None:
-        return None
-    return f"swap_label_no_connect;{changed_thing}"
-
-
-def swap(first_group, second_group) -> str:
     if len(first_group) == 0:
-        return None
-    first_node = choice(first_group)
-    neighbours = second_group.within_reach_of(first_node, 20)
-    if len(neighbours) == 0:
-        # don't do anything in this case
-        return None
-    second_node = choice(neighbours)
+        assert False
+    while True:
+        first_node = choice(first_group)
+        neighbours = second_group.within_reach_of(first_node, NEIGHBOUR_DISTANCE)
+        if len(neighbours) == 0:
+            # don't do anything in this case
+            continue
+        second_node = choice(neighbours)
 
-    # Don't swap with itself
-    if first_node.value == second_node.value:
-        return None
+        # Don't swap with itself
+        if first_node.value == second_node.value:
+            continue
+        break
 
     # swap positions
     first_node_at = tuple(first_node.at)
@@ -59,31 +54,90 @@ def swap(first_group, second_group) -> str:
 
     return f"{first_node.value}-{second_node.value}"
 
-def change_file(file: str) -> str:
-    in_file = f"work_dir/{file}"
+
+def count_matches(schem, first_group_name, second_group_name) -> int:
+    if not hasattr(schem, first_group_name):
+        return 0
+    if not hasattr(schem, second_group_name):
+        return 0
+    first_group = getattr(schem, first_group_name)
+    second_group = getattr(schem, second_group_name)
+
+    def count_matches_for(first_node) -> int:
+        try:
+            return len(second_group.within_reach_of(first_node, NEIGHBOUR_DISTANCE))
+        except:
+            return 0
+    return sum([count_matches_for(first_node) for first_node in first_group])
+
+
+def count_all():
+    if pathlib.Path.exists(pathlib.Path("sch_files.json")):
+        with open("sch_files.json", "r") as f:
+            return json.load(f)
+
+    sch_files_json = {
+            "files": [],
+            "possible_faults": {
+                "delete-wire": [],
+                "swap-label-label": [],
+                "swap-label-hierarchical_label": [],
+                # there are no global labels in this schematic
+                # "swap-label-global_label": [],
+                "swap-label-no_connect": [],
+                "swap-hierarchical_label-hierarchical_label": [],
+                # "swap-hierarchical_label-global_label": [],
+                "swap-hierarchical_label-no_connect": [],
+                # "swap-global_label-global_label": [],
+                # "swap-global_label-no_connect": [],
+                # swap-no_connect-no_connect makes no sense
+            },
+        }
+
+    # ignore snippets (we're not doing unit tests, after all); mechanical components; unused connectors in firmware/schematics; sheets without any digital/analog data line
+    with open("sch_files.txt", "r") as in_file:
+        sch_files = [line.strip() for line in in_file.readlines()]
+
+    for file in sch_files:
+        in_file = f"work_dir/{file}"
+        schem = skip.Schematic(in_file)
+        sch_files_json["files"].append(file)
+        sch_files_json["possible_faults"]["delete-wire"].append(len(schem.wire) if hasattr(schem, "wire") else 0)
+        for key in sch_files_json["possible_faults"].keys():
+            keys = key.split("-")
+            if keys[0] != "swap":
+                continue
+            sch_files_json["possible_faults"][key].append(count_matches(schem, keys[1], keys[2]))
+
+    with open("sch_files.json", "w") as file:
+        json.dump(sch_files_json, file, indent=4)
+    return sch_files_json
+
+
+def main():
+    sch_files_json = count_all()
+    fault_weights = [sum(possible_fault) for possible_fault in sch_files_json["possible_faults"].values()]
+    chosen_fault, counts = choices(list(sch_files_json["possible_faults"].items()), weights=fault_weights, k=1)[0]
+    changed_file = choices(sch_files_json["files"], weights=counts, k=1)[0]
+
+    in_file = f"work_dir/{changed_file}"
     report_dir = f"reports/{sys.argv[1]}"
-    report_file = f"{report_dir}/{file.replace("/", "_")}"
+    report_file = f"{report_dir}/{changed_file.replace("/", "_")}"
 
     schem = skip.Schematic(in_file)
 
-    changed_thing = None
-    while changed_thing is None:
-        changed_thing = choices((delete_wire, swap_labels, swap_label_no_connect), weights=(20, 50, 30), k=1)[0](schem)
+    chosen_faults = chosen_fault.split("-")
+    if chosen_faults[0] == "swap":
+        changed_thing = swap(schem, chosen_faults[1], chosen_faults[2])
+    elif chosen_fault == "delete-wire":
+        changed_thing = delete_wire(schem)
+    else:
+        assert False
 
     schem.write(in_file)
     shutil.copyfile(in_file, report_file)
 
-    return changed_thing
-
-
-def main():
-    # ignore snippets (we're not doing unit tests, after all); mechanical components; unused connectors in firmware/schematics; sheets without any digital/analog data line
-    with open("sch_files.txt", "r") as file:
-        sch_files = [line.strip() for line in file.readlines()]
-    changed_file = choice(sch_files)
-    changed_thing = change_file(changed_file)
-
-    print(f"{changed_file};{changed_thing}")
+    print(f"{changed_file};{chosen_fault};{changed_thing}")
 
 if __name__ == "__main__":
     main()
